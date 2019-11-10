da474e0a287a9aeab952a38444c67a17001745e2
Michelle Tanzil <mtanzil01@gmail.com>
2019-11-10 09:33:57 -0800

    first commit

---
+++ apps/poorly_coded_store/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from .models import Order, Product
+from django.db.models import Sum
+
+def index(request):
+    context = {
+        "all_products": Product.objects.all()
+    }
+    return render(request, "store/index.html", context)
+
+def checkout(request):
+    context = {
+        'order': Order.objects.last(),
+        'total_price_for_all_orders': Order.objects.aggregate(Sum('total_price')),
+        'total_quantity_for_all_orders': Order.objects.aggregate(Sum('quantity_ordered')),
+    }
+    return render(request, "store/checkout.html", context)
+
+def calculations(request): 
+    product = Product.objects.get(id=request.POST['product_id'])
+    price_of_product = product.price
+    quantity_from_form = int(request.POST["quantity"])
+    total_charge = quantity_from_form * price_of_product
+    print("Charging credit card...")
+    Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
+    return redirect('/checkout')
